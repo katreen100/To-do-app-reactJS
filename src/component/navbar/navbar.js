94ba2565576b734fa9de9eb3cc5294901a5739de
katreen100 <katyebraheem@gmail.com>
2022-08-22 11:29:31 +0200

    update-navbar

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -17,9 +17,7 @@ export default function Navbarfun(){
                     <li class="nav-item">
                     <Link class="nav-link" to="/todo">ToDo</Link>
                     </li>
-                    <li class="nav-item">
-                    <Link class="nav-link" to="/transaction">Transaction</Link>
-                    </li>  
+                    
                 </ul>
                 </div>
             </div>
